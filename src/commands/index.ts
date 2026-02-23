4cee55d6ab10942d4cb151208b0a5f160ede8942
Hitesh <shindehitesh26@gmail.com>
2026-02-23 06:28:32 -0600

    Remove unused commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,13 +1,9 @@
 import * as vscode from "vscode";
 import { PanelManager } from "../webview/PanelManager";
-import { ConnectionManager } from "../db/ConnectionManager";
-import { VectorStoreManager } from "../vectorstore/VectorStoreManager";
 
 export function registerCommands(
   context: vscode.ExtensionContext,
-  panelManager: PanelManager,
-  connectionManager: ConnectionManager,
-  vectorStoreManager: VectorStoreManager
+  panelManager: PanelManager
 ): void {
   context.subscriptions.push(
     vscode.commands.registerCommand("schemasight.openPanel", () => {
@@ -16,15 +12,6 @@ export function registerCommands(
 
     vscode.commands.registerCommand("schemasight.addConnection", () => {
       panelManager.openOrReveal();
-      // Panel will handle the add-connection flow via webview UI
-    }),
-
-    vscode.commands.registerCommand("schemasight.crawlSchema", async () => {
-      // TODO: prompt user to pick a connection then trigger crawl
-    }),
-
-    vscode.commands.registerCommand("schemasight.clearIndex", async () => {
-      // TODO: prompt user to pick a connection then clear its index
     })
   );
 }
